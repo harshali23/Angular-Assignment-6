36607704e97ebfea48759e5375ac6bca5f288464
harshali23 <46553270+harshali23@users.noreply.github.com>
2023-12-29 23:38:15 -0500

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'assignment6' title`, () => {
+  it(`should have the 'StringInterpolation' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('assignment6');
+    expect(app.title).toEqual('StringInterpolation');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, assignment6');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, StringInterpolation');
   });
 });
